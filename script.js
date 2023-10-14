9e277cd6815a6ba36250ac13d1a784a16c49a86a
BIBOAR <127908839+musabmohtaha@users.noreply.github.com>
2023-10-14 04:23:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const ideas = {
+	cat1: [
+        "Bibo",
+        "Maiden",
+        "Willow",
+        "Majd",
+        "Madara",
+        "Hadra",
+        "Rania",
+        "Aug",
+        "Jana"
+	],
+	cat2: [
+        "Bibo",
+        "Maiden",
+        "Willow",
+        "Majd",
+        "Madara",
+        "Hadra",
+        "Rania",
+        "Aug",
+        "Jana"
+	]
+	
+};
+
+const allIdeas = [].concat(...Object.values(ideas));
+
+const categories = document.querySelectorAll('#categories a');
+
+categories.forEach(category => {
+	category.addEventListener('click', () => {
+		let idea;
+		if (category.id === 'all') {
+			idea = allIdeas[Math.floor(Math.random() * allIdeas.length)];
+		} else {
+			idea = ideas[category.id][Math.floor(Math.random() * ideas[category.id].length)];
+		}
+		const ideasDiv = document.querySelector('#ideas');
+		ideasDiv.innerHTML = `<h2>${idea}</h2>`;
+	});
+});
+
+     
